45bdfd2f02a900ab40a87ea1310287698c27553b
jeanfour <lec_cos@outlook.es>
2021-08-29 17:00:49 -0500

    cambios en el header, agregado responsive y memu

---
+++ javascript.js
@@ -0,0 +1,13 @@
+const navtoggle = document.querySelector(".toggle")
+const navmenu = document.querySelector(".nav-menu")
+
+navtoggle.addEventListener("click",() => {
+navmenu.classList.toggle("nav-menu_visible");
+
+if(navmenu.classList.contains("nav-menu_visible")){
+    toggle.setAttribute("area-label", "Cerrar menú");
+} else {
+    toggle.setAttribute("arial-label","Abrir menú");
+}
+
+});
